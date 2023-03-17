bcef23bab51508dc5d1adf81f94a5b3d9077ed1e
ohsangsoo <tkdtn3264@gmail.com>
2023-03-17 14:24:15 +0900

    회원 엔티티(등급)

--- src/main/java/oss/core/member/Grade.java
+++ src/main/java/oss/core/member/Grade.java
@@ -1,39 +1,7 @@
 package oss.core.member;
 
 public enum Grade {
-    ; //
-    //BASIC,VIP
-    private Long id;    // 회원 아이디
-    private String name;// 회원 이름
-    private Grade grade;// 회원 등급
+    BASIC,
+    VIP
 
-    Grade(Long id, String name, Grade grade) {
-        this.id = id;
-        this.name = name;
-        this.grade = grade;
-    }
-    //  데이터를 가지고오고 뽑아내는 getter setter
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Grade getGrade() {
-        return grade;
-    }
-
-    public void setGrade(Grade grade) {
-        this.grade = grade;
-    }
 }
