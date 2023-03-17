9b667d43cfd34c521f606d8c6e7af0be6d23dfd2
ohsangsoo <tkdtn3264@gmail.com>
2023-03-17 14:58:21 +0900

    회원 등급 연습하기

---
+++ src/main/java/oss/core/member/Member.java
@@ -0,0 +1,37 @@
+package oss.core.member;
+
+public class Member {
+    private Long id;    // 회원 아이디
+    private String name;// 회원 이름
+    private Grade grade;// 회원 등급
+
+    public Member(Long id, String name, Grade grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+    // 데이터를 가져오고 뽑아내는 getter setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Grade grade) {
+        this.grade = grade;
+    }
+}
