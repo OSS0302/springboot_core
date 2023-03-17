1ce0c81b79ba76d627e853f709ea1b56506f4ddf
ohsangsoo <tkdtn3264@gmail.com>
2023-03-17 15:19:58 +0900

    회원 Service interface  구현체 생성하기

---
+++ src/main/java/oss/core/member/MemberServiceImpl.java
@@ -0,0 +1,16 @@
+package oss.core.member;
+
+public class MemberServiceImpl implements MemberService {
+
+    private  final MemberRepository memberRepository = new MemoryMemberRepository();
+    @Override
+    public void join(Member member) {
+// join에서 save를 호출하면 다형성에 의해서 MemorymemberRepository를 호출하면 MemorymemberRepository 안에 save 가 호출된다.
+    memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
