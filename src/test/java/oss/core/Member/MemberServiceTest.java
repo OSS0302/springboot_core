9a5c43c49a60f3923f855e4c7bedd446a1fed0a2
ohsangsoo <tkdtn3264@gmail.com>
2023-03-17 16:09:46 +0900

    Junit 테스트하기

---
+++ src/test/java/oss/core/Member/MemberServiceTest.java
@@ -0,0 +1,24 @@
+package oss.core.Member;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import oss.core.member.Grade;
+import oss.core.member.Member;
+import oss.core.member.MemberService;
+import oss.core.member.MemberServiceImpl;
+
+public class MemberServiceTest {
+    MemberService memberService = new MemberServiceImpl(); // 필드변수에 Service 구현체를 만들어야한다.
+    @Test
+    void join(){
+        //given
+        Member member =new Member(1L,"memberA", Grade.VIP);// 1번 이름 : memberA Grade등급:vip
+
+        //when
+        memberService.join(member);// 회원가입을 했을때
+        Member findMember = memberService.findMember(1L);
+        //than
+        Assertions.assertThat(member).isEqualTo(findMember);   //인텔리제이에서 사용하는 member 와 findMember가 같은지 확인하는 API
+    }
+}
