77553b7e4dc14f54ac34d3688bc16f10c0e6ea3a
ohsangsoo <tkdtn3264@gmail.com>
2023-03-18 11:50:07 +0900

    주문 서비스 구현체 생성하기

---
+++ src/main/java/oss/core/order/OrderServiceImpl.java
@@ -0,0 +1,20 @@
+package oss.core.order;
+
+import oss.core.discount.DiscountPolicy;
+import oss.core.discount.FixDiscountPolicy;
+import oss.core.member.Member;
+import oss.core.member.MemberRepository;
+import oss.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements  OrderService{
+
+    private  final MemberRepository memberRepository = new MemoryMemberRepository(); // 회원 레포지토리 생성
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();//회원 할인 객체생성
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrince = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId,itemName,itemPrice,discountPrince);
+    }
+}
