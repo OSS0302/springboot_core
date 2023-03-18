9c1714880dcdf5a258979b07cf55741cdcadf1c3
ohsangsoo <tkdtn3264@gmail.com>
2023-03-18 11:49:20 +0900

    주문과 할인 도메인 실행과 테스트

---
+++ src/main/java/oss/core/order/OrderApp.java
@@ -0,0 +1,22 @@
+package oss.core.order;
+
+import oss.core.member.Grade;
+import oss.core.member.Member;
+import oss.core.member.MemberService;
+import oss.core.member.MemberServiceImpl;
+
+public class OrderApp {
+    public static void main(String[] args) {
+    MemberService memberService =new MemberServiceImpl();
+    OrderService orderService =new OrderServiceImpl();
+    
+    Long memberId =1L;
+    Member member =new Member(memberId, "memberA",Grade.VIP);
+    memberService.join(member);
+    
+    Order order = orderService.createOrder(memberId,"itemA",1000);
+
+        System.out.println("order = " + order);
+        //System.out.println("order.calculatePrice() = " + order.calculatePrice());
+    }
+}
