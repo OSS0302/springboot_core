e0b96b25a9d64cfde2db518b5a73db0465c2549c
ohsangsoo <tkdtn3264@gmail.com>
2023-03-27 17:57:48 +0900

    AppConfig 를 순수 자바에서 스프링 부트로변경해준 작업

--- src/main/java/oss/core/AppConfig.java
+++ src/main/java/oss/core/AppConfig.java
@@ -1,5 +1,7 @@
 package oss.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import oss.core.discount.DiscountPolicy;
 import oss.core.discount.FixDiscountPolicy;
 import oss.core.discount.RateDiscountPolicy;
@@ -10,20 +12,22 @@ import oss.core.member.MemoryMemberRepository;
 import oss.core.order.Order;
 import oss.core.order.OrderService;
 import oss.core.order.OrderServiceImpl;
-
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService(){
         return  new MemberServiceImpl(memberRepository()); // 멤버 서비스 구현체에서 만들고   appconfig에서 메모리 멤머 레포지토리를 쓸거야 의존관계 주입
     }
-
-    private static MemberRepository memberRepository() {
+    @Bean
+    public static MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(
                 memberRepository(),discountPolicy());
     }
+    @Bean
     public DiscountPolicy discountPolicy (){
         //return new FixDiscountPolicy(); 고정 할인 정책
         return new RateDiscountPolicy(); // 할인율 정책
