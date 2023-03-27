8823fbe5c86eb5b04e9aeb4e759a92a721b5ad10
ohsangsoo <tkdtn3264@gmail.com>
2023-03-27 16:32:56 +0900

    리펙토리 하기

--- src/main/java/oss/core/AppConfig.java
+++ src/main/java/oss/core/AppConfig.java
@@ -1,6 +1,8 @@
 package oss.core;
 
+import oss.core.discount.DiscountPolicy;
 import oss.core.discount.FixDiscountPolicy;
+import oss.core.member.MemberRepository;
 import oss.core.member.MemberService;
 import oss.core.member.MemberServiceImpl;
 import oss.core.member.MemoryMemberRepository;
@@ -10,11 +12,18 @@ import oss.core.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService(){
-        return  new MemberServiceImpl(new MemoryMemberRepository()); // 멤버 서비스 구현체에서 만들고  appconfig에서 메모리 멤머 레포지토리를 쓸거야 의존관계 주입
-    }
-    public OrderService orderService(){
-        return new OrderServiceImpl(
-                new MemoryMemberRepository(), new FixDiscountPolicy());
+        return  new MemberServiceImpl(memberRepository()); // 멤버 서비스 구현체에서 만들고   appconfig에서 메모리 멤머 레포지토리를 쓸거야 의존관계 주입
     }
 
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService(){
+        return new OrderServiceImpl(
+                memberRepository(),discountPolicy());
+    }
+    public DiscountPolicy discountPolicy (){
+        return new FixDiscountPolicy();
+    }
 }
