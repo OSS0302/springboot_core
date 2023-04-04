94ef5ba34dfc8d10623956fc11de19ed924f737f
ohsangsoo <tkdtn3264@gmail.com>
2023-04-04 19:51:19 +0900

    애플리케이션 빈 출력하기

--- src/test/java/oss/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/oss/core/beanfind/ApplicationContextInfoTest.java
@@ -11,15 +11,30 @@ public class ApplicationContextInfoTest {
 
     @Test
     @DisplayName("모든빈 출력하기")
-    void findBean (){
+    void findBean () {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         // iter누르면 for 문 이 자동으로 생성된다.
         for (String beanDefinitionName : beanDefinitionNames) {
             Object bean = ac.getBean(beanDefinitionName); // 타입을 모르겠떄문에 object으로 만들어진다.
-            System.out.println("name = " + beanDefinitionName +"object = "+bean); // key value
-            
+            System.out.println("name = " + beanDefinitionName + "object = " + bean); // key value
+
 
         }
     }
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기 ")
+    void findApplicationBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition =ac.getBeanDefinition(beanDefinitionName);
 
+            //ROLE_APPLICATION : 일반적으로 사용자가 정의한 빈
+            //ROLE_INFRASTRUCTURE : 스프링이 내부에서 사용하는 빈
+
+            if(beanDefinition.getRole() ==BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName); // 타입을 모르겠떄문에 object으로 만들어진다.
+                System.out.println("name = " + beanDefinitionName + "object = " + bean); // key value
+            }
+        }
+    }
 }
