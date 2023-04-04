814f6c0038b41d123fdcbe42dd67b22c1062f5d4
ohsangsoo <tkdtn3264@gmail.com>
2023-04-04 20:28:38 +0900

    구체 타입으로 조회하기

--- src/test/java/oss/core/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/oss/core/beanfind/ApplicationContextBasicFindTest.java
@@ -3,12 +3,14 @@ package oss.core.beanfind;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import oss.core.AppConfig;
 import oss.core.member.MemberService;
 import oss.core.member.MemberServiceImpl;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationContextBasicFindTest {
     AnnotationConfigApplicationContext ac= new AnnotationConfigApplicationContext(AppConfig.class); // 커맨드 +E 이전으로 돌아가기
@@ -30,5 +32,6 @@ class ApplicationContextBasicFindTest {
         MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
         assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
         //구현에 의존 했으므로 별로 좋은 코딩이 아니다 역할에만 의존하는 것이 좋다.
+        assertThrows(NoSuchBeanDefinitionException.class,()->ac.getBean("xxxxx", MemberService.class));
     }
 }
