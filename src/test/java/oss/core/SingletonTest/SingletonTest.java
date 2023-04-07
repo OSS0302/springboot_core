aef324eb5f93064abde5dc2cb3f52a5d88f944bb
ohsangsoo <tkdtn3264@gmail.com>
2023-04-07 20:17:34 +0900

    스프링 없는 순수한 DI 컨테이너 AssertThat 테스트하기

--- src/test/java/oss/core/SingletonTest/SingletonTest.java
+++ src/test/java/oss/core/SingletonTest/SingletonTest.java
@@ -1,9 +1,14 @@
 package oss.core.SingletonTest;
 
+
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import oss.core.AppConfig;
 import oss.core.member.MemberService;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class SingletonTest {
 
@@ -19,10 +24,18 @@ public class SingletonTest {
 
         //3. 조회: 호출할때 미디 객체를 생성
         MemberService memberService3 = appConfig.memberService();
-
+        // 참조값이 다른 것을 확인 한다.
         System.out.println("memberService1 = " + memberService1);
         System.out.println("memberService2 = " + memberService2);
         System.out.println("memberService3 = " + memberService3);
+
+        //테스트 자동화를 하기위해서
+        // memberService1!=  memberService2
+        assertThat(memberService1).isNotSameAs(memberService2);
+
+        // memberService2!=  memberService3
+        assertThat(memberService2).isNotSameAs(memberService3);
+
 
     }
 }
