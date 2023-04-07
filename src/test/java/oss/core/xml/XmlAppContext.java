3af6b11eb46defa13dec405f93c9698f1b050a6a
ohsangsoo <tkdtn3264@gmail.com>
2023-04-07 19:03:29 +0900

    GenericXmlApplicationContext변수명으로 변경하기

--- src/test/java/oss/core/xml/XmlAppContext.java
+++ src/test/java/oss/core/xml/XmlAppContext.java
@@ -16,7 +16,7 @@ public class XmlAppContext {
 
         @Test
         void xmlAppContext(){
-            ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+            GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
             MemberService memberService =ac.getBean("memberService",MemberService.class);
             assertThat(memberService).isInstanceOf(MemberService.class);
 
