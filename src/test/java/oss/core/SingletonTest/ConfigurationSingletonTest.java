4180240c4ef54b1417944706847929fe24783735
ohsangsoo <tkdtn3264@gmail.com>
2023-04-10 17:51:28 +0900

    검증하기

--- src/test/java/oss/core/SingletonTest/ConfigurationSingletonTest.java
+++ src/test/java/oss/core/SingletonTest/ConfigurationSingletonTest.java
@@ -1,5 +1,6 @@
 package oss.core.SingletonTest;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -16,7 +17,7 @@ public class ConfigurationSingletonTest {
 
         MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class); // 멤버 서비스
         OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);// 오더 서비스
-
+        MemberRepository memberRepository =ac.getBean("memberRepository", MemberRepository.class);
         // 두개 오더 서비스 와 멤버서비스의  인스턴스 가 같는지 비교하기
         MemberRepository memberRepository1 = memberService.getMemberRepository();
         MemberRepository memberRepository2 = orderService.getMemberRepository();
@@ -24,23 +25,12 @@ public class ConfigurationSingletonTest {
         // 출력하기
         System.out.println("memberService-> memberRepository = " + memberRepository1);
         System.out.println("orderService ->memberRepository2 = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
 
-    }@Test
-        void configurationTest1() {
-            ApplicationContext ac = new
-                    AnnotationConfigApplicationContext(AppConfig.class);
-            MemberServiceImpl memberService = ac.getBean("memberService",
-                    MemberServiceImpl.class);
-            OrderServiceImpl orderService = ac.getBean("orderService",
-                    OrderServiceImpl.class);
-            MemberRepository memberRepository = ac.getBean("memberRepository",
-                    MemberRepository.class);
-//모두 같은 인스턴스를 참고하고 있다.
-            System.out.println("memberService -> memberRepository = " +
-                    memberService.getMemberRepository());
-            System.out.println("orderService -> memberRepository  = " +
-                    orderService.getMemberRepository());
-            System.out.println("memberRepository = " + memberRepository);
+        //겁증하기
+        Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository); // 멤버 서비스 와 멤버 리포지토리 가 같은지 확인
+        Assertions.assertThat(orderService.getMemberRepository()).isSameAs((memberRepository));// 오더 서비스와 멤버 리포지토리 가 같은지 확인
+
     }
 
 }
