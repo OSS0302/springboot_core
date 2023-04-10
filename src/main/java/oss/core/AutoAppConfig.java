2b46bc568f9ac610abd537ac898a84b89d64cda1
ohsangsoo <tkdtn3264@gmail.com>
2023-04-10 20:51:02 +0900

    AutoConfig 설명 주석 추가하기

--- src/main/java/oss/core/AutoAppConfig.java
+++ src/main/java/oss/core/AutoAppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(//전체 다  컴포넌트 스캔해서  필요한 부분만 스프링부트 빈으로 자동등록하겠다.전
-        //classes = Configuration.class를 빼겠다 (지정해서 뺸다)
+        //classes = Configuration.class를 빼겠다 (지정해서 뺸다) 왜냐하면 @configuration 안에 ComponentScan이 있기때문에
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
