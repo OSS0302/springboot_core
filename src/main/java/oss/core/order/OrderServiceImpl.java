f5d2f348d7ab3adc23e6afdcc59a25c99ecff03f
ohsangsoo <tkdtn3264@gmail.com>
2023-04-10 20:55:59 +0900

    @Compnent 자동으로 빈 등록되고 생성자위에 @Autowired 를 쓰면 의존관계 주입이 됩니다..

--- src/main/java/oss/core/order/OrderServiceImpl.java
+++ src/main/java/oss/core/order/OrderServiceImpl.java
@@ -1,17 +1,19 @@
 package oss.core.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import oss.core.discount.DiscountPolicy;
 
 import oss.core.member.Member;
 import oss.core.member.MemberRepository;
 import oss.core.member.MemoryMemberRepository;
-
+@Component
 public class OrderServiceImpl implements  OrderService {
     //추상에만 의존해야하는데 구현에도 의존하니까 DIP위반이다.
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();//회원 고정할인 객체생성
     private final MemberRepository memberRepository; // 회원 레포지토리 생성
     private final DiscountPolicy discountPolicy;// 회원 할인 객체 변경하기
-
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
