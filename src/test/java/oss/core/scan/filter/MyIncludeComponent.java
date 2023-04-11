2733e3fa936cf54e982f3ddb000488157953148a
ohsangsoo <tkdtn3264@gmail.com>
2023-04-11 20:00:54 +0900

    컴포넌트 스캔 대상에서 추가할 애노테이션

--- src/test/java/oss/core/scan/filter/MyIncludeComponent.java
+++ src/test/java/oss/core/scan/filter/MyIncludeComponent.java
@@ -5,5 +5,5 @@ import java.lang.annotation.*;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface MyIncludeComponent {
+public @interface MyIncludeComponent { // 컴포넌트 스켄에서 추가한다.
 }
