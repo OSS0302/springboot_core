ff96e07c3b3f4f4edb9f104dd3a1ac8953a69294
ohsangsoo <tkdtn3264@gmail.com>
2023-04-12 18:27:06 +0900

    수정자 주입(setter 주입) setter라 불리는 필드의 값을 변경하는 수정자 메서드를 통해서 의존관계를 주입하는 방법이다.

--- src/main/java/oss/core/order/OrderServiceImpl.java
+++ src/main/java/oss/core/order/OrderServiceImpl.java
@@ -11,14 +11,19 @@ import oss.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements  OrderService {
     //추상에만 의존해야하는데 구현에도 의존하니까 DIP위반이다.
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();//회원 고정할인 객체생성
-    private final MemberRepository memberRepository; // 회원 레포지토리 생성 final 사용하면 외부에서 null로 들어갈 수 있지만 값이  있어야한다.
-    private final DiscountPolicy discountPolicy;// 회원 할인 객체 변경하기
-    // final 사용하면 외부에서 null로 들어갈 수 있지만 값이  있어야한다.  없으면 오류 난다.
-
-     // 생성자 가 한개 인경우에만 @Autowired 생략이 가능하다. 일반적으로 문서에 null 있어도됩니다. 하지 않은 경우를 제외 하고 생성자 값이 있어야한다.
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        System.out.println("memberRepository = " + memberRepository);
+    private  MemberRepository memberRepository; // 회원 레포지토리 생성
+    private  DiscountPolicy discountPolicy;// 회원 할인 객체 변경하기//
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
         System.out.println("discountPolicy = " + discountPolicy);
+        this.discountPolicy = discountPolicy;
+    }
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        System.out.println("memberRepository = " + memberRepository);
+        this.memberRepository = memberRepository;
+    }// 생성자 가 한개 인경우에만 @Autowired 생략이 가능하다. 일반적으로 문서에 null 있어도됩니다. 하지 않은 경우를 제외 하고 생성자 값이 있어야한다.
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
