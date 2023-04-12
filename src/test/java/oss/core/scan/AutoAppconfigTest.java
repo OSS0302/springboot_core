d1d942fa973fefae968b4982adee2f2ef3b8c92f
ohsangsoo <tkdtn3264@gmail.com>
2023-04-12 18:58:59 +0900

    필드 @Autowired 추가하기

--- src/test/java/oss/core/scan/AutoAppconfigTest.java
+++ src/test/java/oss/core/scan/AutoAppconfigTest.java
@@ -4,7 +4,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import oss.core.AutoAppConfig;
+import oss.core.member.MemberRepository;
 import oss.core.member.MemberService;
+import oss.core.order.OrderServiceImpl;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -15,8 +17,11 @@ public class AutoAppconfigTest {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
         // 조회하기
         MemberService memberService = ac.getBean(MemberService.class);
-
         // 검증하기
         assertThat(memberService).isInstanceOf(MemberService.class);
+        // getMemberRepository()에값이 있는지 확인 
+        OrderServiceImpl bean = ac.getBean(OrderServiceImpl.class);
+        MemberRepository memberRepository = bean.getMemberRepository();
+        System.out.println("memberRepository = " + memberRepository);
     }
 }
