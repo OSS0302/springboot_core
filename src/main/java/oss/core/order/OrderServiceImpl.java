2b1cd10486e5d5d16c2ce3e73e2c3fa27fd77f90
ohsangsoo <tkdtn3264@gmail.com>
2023-04-15 20:25:33 +0900

    생성자 한개인 경우에는 @AutoWried를 생략이 가능하다

--- src/main/java/oss/core/order/OrderServiceImpl.java
+++ src/main/java/oss/core/order/OrderServiceImpl.java
@@ -10,13 +10,16 @@ import oss.core.member.MemoryMemberRepository;
 @Component
 public class OrderServiceImpl implements  OrderService {
     //추상에만 의존해야하는데 구현에도 의존하니까 DIP위반이다.//private final DiscountPolicy discountPolicy = new FixDiscountPolicy();//회원 고정할인 객체생성
-     private  MemberRepository memberRepository; // NullPointExceptions // 회원 레포지토리 생성 // Field injection is not recommended
-     private  DiscountPolicy discountPolicy;// 회원 할인 객체 변경하기
+     private final MemberRepository memberRepository; // NullPointExceptions // 회원 레포지토리 생성 // Field injection is not recommended
+     private final DiscountPolicy discountPolicy;// 회원 할인 객체 변경하기 //
+    // final키워드생성자 주입을 사용하면 필드에final키워드를 사용할 수 있다.
+    // 그래서 생성자에서 혹시라도 값이 설정되지 않는 오류를 컴파일 시점에 막아준다
+
 
    // 생성자 가 한개 인경우에만 @Autowired 생략이 가능하다. 일반적으로 문서에 null 있어도됩니다. 하지 않은 경우를 제외 하고 생성자 값이 있어야한다.
     // 수정자 주입 다른 개발자 필요하면 수정해서 쓸수있는 있으니 생성자 주입을 사용하자 // 테스트 누락을 위해서 수정자 주입을 쓴다.
 
-    @Autowired
+    // 생성자 한개인 경우에는 @AutoWried를 생략이 가능하다
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
