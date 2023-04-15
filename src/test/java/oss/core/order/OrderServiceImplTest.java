c5284b4355e292a0e11ab8832a0b01603d1cdd5e
ohsangsoo <tkdtn3264@gmail.com>
2023-04-15 20:09:58 +0900

    순수한 자바 테스트하고 검증하기

--- src/test/java/oss/core/order/OrderServiceImplTest.java
+++ src/test/java/oss/core/order/OrderServiceImplTest.java
@@ -1,11 +1,13 @@
 package oss.core.order;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import oss.core.discount.FixDiscountPolicy;
 import oss.core.member.Grade;
 import oss.core.member.Member;
 import oss.core.member.MemoryMemberRepository;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceImplTest {
@@ -13,10 +15,14 @@ class OrderServiceImplTest {
 
     @Test
     void createOrder(){
+        // 자바 스프링 을 이용하지 않고 이건 순수한 자바이용해서 테스트한것이다..
         MemoryMemberRepository MemberRepository = new MemoryMemberRepository();
-        MemberRepository.save(new Member(1L,"name", Grade.VIP));
+        MemberRepository.save(new Member(1L,"name", Grade.VIP)); //테스트를 위한 임의의 회원 정보 생성
         OrderServiceImpl orderService =new OrderServiceImpl(MemberRepository,new FixDiscountPolicy());
-        orderService.createOrder(1L,"itemA",10000);
+        // 조회하기
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        //검증하기
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
 
     }
 }
