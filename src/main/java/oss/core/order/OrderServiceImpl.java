1e1e43df5fa3ebed8231d46b2826852c31934d2a
ohsangsoo <tkdtn3264@gmail.com>
2023-04-15 19:37:26 +0900

    테스트를 위해서 수정하기 위해서 생성자 주입-> setter 수정자 주입으로 변경

--- src/main/java/oss/core/order/OrderServiceImpl.java
+++ src/main/java/oss/core/order/OrderServiceImpl.java
@@ -10,16 +10,24 @@ import oss.core.member.MemoryMemberRepository;
 @Component
 public class OrderServiceImpl implements  OrderService {
     //추상에만 의존해야하는데 구현에도 의존하니까 DIP위반이다.//private final DiscountPolicy discountPolicy = new FixDiscountPolicy();//회원 고정할인 객체생성
-
-     private  MemberRepository memberRepository; // 회원 레포지토리 생성 // Field injection is not recommended
+     private  MemberRepository memberRepository; // NullPointExceptions // 회원 레포지토리 생성 // Field injection is not recommended
      private  DiscountPolicy discountPolicy;// 회원 할인 객체 변경하기
 
    // 생성자 가 한개 인경우에만 @Autowired 생략이 가능하다. 일반적으로 문서에 null 있어도됩니다. 하지 않은 경우를 제외 하고 생성자 값이 있어야한다.
+    // 수정자 주입 다른 개발자 필요하면 수정해서 쓸수있는 있으니 생성자 주입을 사용하자 // 테스트 누락을 위해서 수정자 주입을 쓴다.
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
+    public void setMemberRepository(MemberRepository memberRepository){
+        this.memberRepository =memberRepository;
+    }
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy){
         this.discountPolicy = discountPolicy;
     }
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
 
     @Override
