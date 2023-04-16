5f3715429109c562b412b87112e2d7d4c3c93fb8
ohsangsoo <tkdtn3264@gmail.com>
2023-04-16 19:01:04 +0900

    lombok 테스트하기

---
+++ src/main/java/oss/core/order/HelloLombok.java
@@ -0,0 +1,23 @@
+package oss.core.order;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloLombok {
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        // 인스턴스 생성
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setName("member1");
+        //출력하기
+        String name  = helloLombok.getName();
+        System.out.println("name = " + name);
+
+
+    }
+
+}
