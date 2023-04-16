e295ce5aff69cad36a020dbfa620f98294614885
ohsangsoo <tkdtn3264@gmail.com>
2023-04-16 19:04:33 +0900

    lombok ToString 테스트하기

--- src/main/java/oss/core/order/HelloLombok.java
+++ src/main/java/oss/core/order/HelloLombok.java
@@ -2,9 +2,11 @@ package oss.core.order;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class HelloLombok {
     private String name;
     private int age;
@@ -14,10 +16,8 @@ public class HelloLombok {
         HelloLombok helloLombok = new HelloLombok();
         helloLombok.setName("member1");
         //출력하기
-        String name  = helloLombok.getName();
-        System.out.println("name = " + name);
-
-
+//        String name  = helloLombok.getName();
+//        System.out.println("name = " + name);
+        System.out.println("helloLombok = " + helloLombok);
     }
-
 }
