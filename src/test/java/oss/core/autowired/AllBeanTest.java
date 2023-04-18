522cedf6afbed51ec494ea0bcf679f379a79a111
ohsangsoo <tkdtn3264@gmail.com>
2023-04-18 15:15:54 +0900

    AllbeanTest 빈 등록하기

--- src/test/java/oss/core/autowired/AllBeanTest.java
+++ src/test/java/oss/core/autowired/AllBeanTest.java
@@ -1,8 +1,10 @@
 package oss.core.autowired;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import oss.core.AutoAppConfig;
 import oss.core.discount.DiscountPolicy;
 
 import java.util.List;
@@ -10,14 +12,14 @@ import java.util.Map;
 
 public class AllBeanTest  {
     @Test
-    void findAllBean(){
-        ApplicationContext ac = new AnnotationConfigApplicationContext(DiscountPolicy.class);
+      void findAllBean(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class,DiscountPolicy.class);
 
     }
     static class DiscountPolicy{
         private  final  Map<String, DiscountPolicy> policyMap;
         private  final List<DiscountPolicy>policies;
-
+        @Autowired
         public DiscountPolicy(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
             this.policyMap = policyMap;
             this.policies = policies;
