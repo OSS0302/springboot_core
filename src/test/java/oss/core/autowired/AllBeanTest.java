554b95341bcef695c9addb8b34d86c9d9d85657c
ohsangsoo <tkdtn3264@gmail.com>
2023-04-20 16:23:01 +0900

    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")

--- src/test/java/oss/core/autowired/AllBeanTest.java
+++ src/test/java/oss/core/autowired/AllBeanTest.java
@@ -38,6 +38,8 @@ public class AllBeanTest {
     static class DiscountService{
         private  final Map<String , DiscountPolicy> policyMap;
         private  final List<DiscountPolicy>policies;
+
+        @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
         @Autowired
         public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
             this.policyMap = policyMap;
