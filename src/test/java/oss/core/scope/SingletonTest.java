b38bb912fbd665a16aa7192b5e1628b95f15a633
ohsangsoo <tkdtn3264@gmail.com>
2023-04-20 15:14:10 +0900

    컴포넌트 스캔 자동 등록

--- src/test/java/oss/core/scope/SingletonTest.java
+++ src/test/java/oss/core/scope/SingletonTest.java
@@ -10,9 +10,8 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 @Component
-@Scope("singleton")
+@Scope("singleton")//컴포넌트 스캔 자동 등록
 public class SingletonTest {
-
     @Test
     void singletonBeanFind(){
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SinglentonBean.class);
@@ -23,20 +22,17 @@ public class SingletonTest {
         System.out.println("singlentonBean2 = " + singlentonBean2);
         // 검증 하기
         Assertions.assertThat(singlentonBean1).isSameAs(singlentonBean2); // 같은지 비교 하는
+        ac.close();
     }
     @Scope("singleton") // 수동등록
     static class SinglentonBean{
         @PostConstruct
         public void init(){
             System.out.println("SingletonBean.init");
-
         }
-
         @PreDestroy
         public void destory(){
             System.out.println("SinglentonBean.destory");
         }
-
-
     }
 }
