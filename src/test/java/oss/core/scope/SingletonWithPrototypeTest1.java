d7b76addb691c079dbd1f870d7204935a8d913fb
ohsangsoo <tkdtn3264@gmail.com>
2023-04-20 15:55:23 +0900

    스프링 컨테이너에 프로토타입 빈 직접 요청되면 1이 증가가 되는 지 확인 하는 테스트하기

---
+++ src/test/java/oss/core/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,46 @@
+package oss.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class SingletonWithPrototypeTest1 {
+    @Test
+    void prototypeFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.addCount();
+        Assertions.assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean2.addCount();
+        Assertions.assertThat(prototypeBean2.getCount()).isEqualTo(1);
+
+        ac.close();
+    }
+    @Scope("prototype")
+        static class PrototypeBean{
+            private  int count =0; // 0으로 초기화
+
+            public void addCount(){
+                count++;
+            }
+            public int getCount(){
+                return count;
+
+        }
+        @PostConstruct
+        public void  init(){
+            System.out.println("PrototypeBean.init");
+        }
+        @PreDestroy
+        public void destroy(){
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+
+}
