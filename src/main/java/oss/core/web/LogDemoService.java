6dc2b3011b1add2fa7bd870d492d2574a55a773d
ohsangsoo <tkdtn3264@gmail.com>
2023-04-21 16:51:12 +0900

    로그 서비스  스코프와ObjectProvider 생성하기

--- src/main/java/oss/core/web/LogDemoService.java
+++ src/main/java/oss/core/web/LogDemoService.java
@@ -1,6 +1,7 @@
 package oss.core.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 import oss.core.common.MyLogger;
 
@@ -8,10 +9,11 @@ import oss.core.common.MyLogger;
 @RequiredArgsConstructor // 의존성 주입
 public class LogDemoService {
 
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider; //서비스 도 마찬가지 myLoggerObjectProvider으로 변경해준다.
 
 
     public void logic(String id){
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
         myLogger.log("service id = "+id);
     }
 }
