abf683f9d579b2efc6d985b59a48c4793715585e
ohsangsoo <tkdtn3264@gmail.com>
2023-04-21 17:13:24 +0900

    @Scope proxyMode = ScopedProxyMode.TARGET_CLASS MyLogger의 가짜 프록시 클래스를 만들어두고 HTTP request와 상관 없이 가짜 프록시클래스를 다른 빈에 미리 주입해 둘 수있다.

--- src/main/java/oss/core/common/MyLogger.java
+++ src/main/java/oss/core/common/MyLogger.java
@@ -2,6 +2,7 @@ package oss.core.common;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -9,7 +10,7 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request") //request 스코프로 지정했다. 이제 이 빈은 HTTP 요청 당 하나씩 생성되고, HTTP 요청이 끝나는 시점에 소멸된다.
+@Scope(value = "request",proxyMode = ScopedProxyMode.TARGET_CLASS) //MyLogger의 가짜 프록시 클래스를 만들어두고 HTTP request와 상관 없이 가짜 프록시클래스를 다른 빈에 미리 주입해 둘 수있다.
 public class MyLogger {
     private String uuid;
     private String requestURL;
