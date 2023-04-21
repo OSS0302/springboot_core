f6d7ef76f211c3c4ad4eff6e0cb56c273fcbf519
ohsangsoo <tkdtn3264@gmail.com>
2023-04-21 16:50:39 +0900

    로그 컨트롤러 ObjectProvider 생성하기

--- src/main/java/oss/core/web/LogDemoController.java
+++ src/main/java/oss/core/web/LogDemoController.java
@@ -1,8 +1,8 @@
 package oss.core.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import oss.core.common.MyLogger;
@@ -14,20 +14,18 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider; // myLogger를 주입 받는게 아니라 myLogger를 찾을 수있는 디펜던시 로그가 주입된다.
 
 
     @RequestMapping("log-demo")
     @ResponseBody // 문자 그대로 응답을 보낼 수있다.
     public String logDemo(HttpServletRequest request) {
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
         String requestURL = request.getRequestURI().toString();
         myLogger.setRequestURL(requestURL); // URL 정보 보내기
 
         myLogger.log("controller test");
         logDemoService.logic("testId"); // 서비스 호출
         return "OK";
-
-
     }
-
 }
