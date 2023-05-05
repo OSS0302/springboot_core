62fc8288a3c56429d26f740b300fd662fb93b2fc
ohsangsoo <tkdtn3264@gmail.com>
2023-05-05 21:13:55 +0900

    로그를 찍어서 확인한다.

--- src/main/java/oss/core/annotation/MainDiscountPolicy.java
+++ src/main/java/oss/core/annotation/MainDiscountPolicy.java
@@ -8,6 +8,6 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 @Documented
-@Qualifier("mainDiscountPolicy ")
+@Qualifier("mainDiscountPolicy") //@Qualifier("mainDiscountPolicy") 처럼, 문자를 직접 적어주는 방식은 컴파일 시점에 체크가 되지 않습니다.
 public @interface MainDiscountPolicy {
 }
